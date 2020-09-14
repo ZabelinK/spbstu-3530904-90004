3d32b8bf64aee9ffa794bde415c2cd35c26a3a1f
Dmitry Shabinsky <sdimosikvip@gmail.com>
2020-09-14 20:38:11 +0700

    change

--- lesson_1/example.cpp
+++ lesson_1/example.cpp
@@ -2,33 +2,37 @@
 #include <cstdio>
 #include <stdexcept>
 
-struct user_t {
-    int id; 
-    char * login; 
-    char * first_name; 
-    char * last_name; 
-    time_t last_login; 
+struct user_t
+{
+  int id;
+  char *login;
+  char *first_name;
+  char *last_name;
+  time_t last_login;
 };
 
-extern "C" int read_user(FILE *, struct user_t *); 
+extern "C" int read_user(FILE *, user_t *user);
 
-user_t last_logged_in(FILE * source) 
-{ 
-    std::time_t last_login = 0; 
-    user_t last_user; 
-    
-    while (!feof(source)) { 
-        user_t user; 
+user_t last_logged_in(FILE *source)
+{
+  std::time_t last_login = 0;
+  user_t *last_user = nullptr;
 
-        if (!read_user(source, &user)) { 
-            throw std::runtime_error("Read failed"); 
-        } 
+  while (!feof(source))
+  {
+    user_t *user = nullptr;
 
-        if (last_login < user.last_login) {
-            last_login = user.last_login; 
-            last_user = user; 
-        } 
+    if (!read_user(source, user))
+    {
+      throw std::runtime_error("Read failed");
     }
-    
-    return last_user; 
+
+    if (last_login < user->last_login)
+    {
+      last_login = user->last_login;
+      last_user = user;
+    }
+  }
+
+  return *last_user;
 }
