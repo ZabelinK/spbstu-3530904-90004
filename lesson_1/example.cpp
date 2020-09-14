d83487c2f035a4d32bc65c4e4e93a49a0c2e6be0
bromles <ushakov3.av@edu.spbstu.ru>
2020-09-14 17:14:40 +0300

    Fix errors:
    1. Unnecessary last_login variable
    2. Wrong comparison
    
    Found errors:
    
    1. Memory leak due to no deallocation in the structure
    2. No checking for the existence and correctness of the file

--- lesson_1/example.cpp
+++ lesson_1/example.cpp
@@ -1,30 +1,39 @@
-struct user_t { 
-    int id; 
-    char * login; 
-    char * first_name; 
-    char * last_name; 
-    time_t last_login; 
+#include <ctime>
+#include <cstdio>
+#include <stdexcept>
+
+struct user_t
+{
+  int id;
+  char *login;
+  char *first_name;
+  char *last_name;
+  time_t last_login;
 };
 
-extern "C" int read_user(FILE *, struct user_t *); 
+extern "C" int read_user(FILE *, struct user_t *);
 
-user_t last_logged_in(FILE * source) 
-{ 
-    std::time_t last_login = 0; 
-    user_t last_user; 
-    
-    while (!feof(source)) { 
-        user_t user; 
-        
-        if (!read_user(source, &user)) { 
-            throw std::runtime_error("Read failed"); 
-        } 
-        
-        if (last_login < user.last_login) {
-            last_login = user.last_login; 
-            last_user = user; 
-        } 
+user_t last_logged_in(FILE *source)
+{
+  user_t last_user;
+
+  last_user.last_login = 0;
+
+  while (!feof(source))
+  {
+    user_t user;
+
+    if (!read_user(source, &user))
+    {
+      throw std::runtime_error("Read failed");
     }
-    
-    return last_user; 
+
+    if (last_user.last_login <= user.last_login)
+    {
+      last_user = user;
+    }
+  }
+
+  return last_user;
 }
+
